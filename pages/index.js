17355ff9b84a4717ef78bdca261e27bda01f006f
Oluwatobi Adesanya <104910188+theChosenDevop@users.noreply.github.com>
2022-11-07 19:20:33 +0100

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Pizza Restaurant In Lagos</title>
+        <meta
+          name="description"
+          content="Best Pizza shop in Central Business District"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+}
